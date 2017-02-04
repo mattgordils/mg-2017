3c633c2883b80032d8b6fa7053199400d158fb99
Matt Gordils <mattgordils@gmail.com>
2017-02-04 12:12:01 -0500

    Static site

--- site/snippets/projects-icon.php
+++
@@ -1,3 +0,0 @@
-<svg width="16px" height="16px" viewBox="0 0 16 16">
-  <path d="M0,6 L4,6 L4,10 L0,10 L0,6 Z M0,0 L10,0 L10,4 L0,4 L0,0 Z M6,6 L10,6 L10,10 L6,10 L6,6 Z M12,6 L16,6 L16,10 L12,10 L12,6 Z M12,0 L16,0 L16,4 L12,4 L12,0 Z M6,12 L16,12 L16,16 L6,16 L6,12 Z M0,12 L4,12 L4,16 L0,16 L0,12 Z" id="Combined-Shape" stroke="none" fill="#000000" fill-rule="evenodd"></path>
-</svg>
